023521de89a9f07e1ad3c651e47fb54db2ebd9e3
mjbondra <mjbondra@umich.edu>
2013-12-13 18:33:55 -0500

    consolidate prints

--- server.dart
+++ server.dart
@@ -4,8 +4,7 @@ import 'dart:io';
 main() {
   HttpServer.bind('127.0.0.1', 3000).then((server){
     server.listen((HttpRequest request){
-      print(request.method);
-      print(request.uri);
+      print('${request.method} ${request.uri}');
       request.response.write('Hello, this is dart:io in action!');
       request.response.close();
     });
